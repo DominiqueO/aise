404519249a9cd7e3d0b9dd73d84840fb34fdd3d9
DominiqueO <DominiqueO@users.noreply.github.com>
2025-01-19 12:50:07 +0100

    Moved training to function in separate file such that training loops don't have to be written for every model anew

---
+++ src/training/Training.py
@@ -0,0 +1,36 @@
+
+import torch
+
+def train_NN(model, optimizer, scheduler, loss, training_set, testing_set, epochs=50, freq_print=1, save=False,
+             save_name='default'):
+    loss_history = []
+    for epoch in range(epochs):
+        train_mse = 0.0
+        for step, (input_batch, output_batch) in enumerate(training_set):
+            optimizer.zero_grad()
+            output_pred_batch = model(input_batch).squeeze(2)
+            loss_f = loss(output_pred_batch, output_batch)
+            loss_f.backward()
+            optimizer.step()
+            train_mse += loss_f.item()
+        train_mse /= len(training_set)
+
+        scheduler.step()
+
+        with torch.no_grad():
+            model.eval()
+            test_relative_l2 = 0.0
+            for step, (input_batch, output_batch) in enumerate(testing_set):
+                output_pred_batch = model(input_batch).squeeze(2)
+                loss_f = (torch.mean((output_pred_batch - output_batch) ** 2) / torch.mean(
+                    output_batch ** 2)) ** 0.5 * 100
+                test_relative_l2 += loss_f.item()
+            test_relative_l2 /= len(testing_set)
+
+        if epoch % freq_print == 0: print("######### Epoch:", epoch, " ######### Train Loss:", train_mse,
+                                          " ######### Relative L2 Test Norm:", test_relative_l2)
+        loss_history.append(train_mse)
+
+    if save:
+        torch.save(model.state_dict(), save_name)
+    return model, loss_history, test_relative_l2
--- src/training/task1.py
+++ src/training/task1.py
@@ -12,6 +12,7 @@ from torch.optim import Adam
 import matplotlib.pyplot as plt
 
 from src.models.FNO import FNO1d
+import src.training.Training as Train
 
 data_path = "../../data/task1/"
 torch.manual_seed(0)
@@ -62,30 +63,8 @@ optimizer = Adam(fno.parameters(), lr=learning_rate, weight_decay=1e-5)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 l = torch.nn.MSELoss()
 freq_print = 1
-for epoch in range(epochs):
-    train_mse = 0.0
-    for step, (input_batch, output_batch) in enumerate(training_set):
-        optimizer.zero_grad()
-        output_pred_batch = fno(input_batch).squeeze(2)
-        loss_f = l(output_pred_batch, output_batch)
-        loss_f.backward()
-        optimizer.step()
-        train_mse += loss_f.item()
-    train_mse /= len(training_set)
 
-    scheduler.step()
-
-    with torch.no_grad():
-        fno.eval()
-        test_relative_l2 = 0.0
-        for step, (input_batch, output_batch) in enumerate(testing_set):
-            output_pred_batch = fno(input_batch).squeeze(2)
-            loss_f = (torch.mean((output_pred_batch - output_batch) ** 2) / torch.mean(output_batch ** 2)) ** 0.5 * 100
-            test_relative_l2 += loss_f.item()
-        test_relative_l2 /= len(testing_set)
-
-    if epoch % freq_print == 0: print("######### Epoch:", epoch, " ######### Train Loss:", train_mse, " ######### Relative L2 Test Norm:", test_relative_l2)
-
+fno, loss_history, relative_l2 = Train.train_NN(fno, optimizer, scheduler, l, training_set, testing_set, epochs, freq_print)
 
 # Plot predicted function for visual check
 idx_data = 10
