32fce3fec0bd3d4caaa03a6d421684c3c2e00a7c
DominiqueO <DominiqueO@users.noreply.github.com>
2025-01-21 19:59:06 +0100

    Changed activation functions

--- src/models/FuncApprox.py
+++ src/models/FuncApprox.py
@@ -10,13 +10,13 @@ class FuncApprox(nn.Module):
         super(FuncApprox, self).__init__()
         self.network = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),  # Input layer: (x, y, t)
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, output_dim)  # Output layer: u(x, y, t)
         )
